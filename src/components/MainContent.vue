5e4dd90770c9c24b26d9445c3dd1f98e2e5f30f3
yukihane <yukihane.feather@gmail.com>
2018-02-03 23:14:54 +0900

    メインコンテンツのスケルトン

---
+++ src/components/MainContent.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1>このサイト</h1>
+    <div>
+        現在フリーランスでソフトウェアエンジニアをやっている人のウェブサイトです。
+        学習も兼ねてJavaScriptフレームワークを用いて本サイトを作成しています。
+    </div>
+    <h1>コンテンツ</h1>
+    <ul>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/wiki">Wiki</a></li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MainContent'
+}
+</script>
